7725806d3d9bcb7319e113a52b0ff512ef1befe1
dlv237 <dolobos@uc.cl>
2024-06-12 20:26:10 -0400

    S3 get/post logic

--- my-app/src/pages/api/s3-get-url.ts
+++ my-app/src/pages/api/s3-get-url.ts
@@ -1,11 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import aws from 'aws-sdk';
+import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
-const s3 = new aws.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+const s3Client = new S3Client({
   region: process.env.AWS_REGION,
-  signatureVersion: 'v4',
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+  },
 });
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -15,11 +17,11 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const params = {
       Bucket: process.env.AWS_S3_BUCKET_NAME,
       Key: key as string,
-      Expires: 60,
     };
 
     try {
-      const url = await s3.getSignedUrlPromise('getObject', params);
+      const command = new GetObjectCommand(params);
+      const url = await getSignedUrl(s3Client, command, { expiresIn: 60 });
       res.status(200).json({ url });
     } catch (error) {
       console.error(error);
