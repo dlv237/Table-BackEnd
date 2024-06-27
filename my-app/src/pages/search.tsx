b9b843c323b2cc4d4dee5eb59cceff85732e7ba3
dlv237 <dolobos@uc.cl>
2024-06-27 01:03:57 -0400

    fix

--- my-app/src/pages/search.tsx
+++ my-app/src/pages/search.tsx
@@ -21,27 +21,6 @@ export default function Search() {
                     <div className="logoContainerSmall" style={{ zIndex: "1", cursor: "pointer" }} onClick={() => window.location.href = '/'}>
                         <img src="/LOGO_TEXTO.png" alt="Logo" className="centeredImage" />
                     </div>
-                    <div style={{
-                        position: 'absolute',
-                        top: '50%',
-                        left: '50%',
-                        transform: 'translate(-50%, -30vh)',
-                        fontSize: '20px',
-                        cursor: 'pointer'
-                    }}>
-                        buscar arquitectos
-                    </div>
-                    <div style={{
-                        position: 'absolute',
-                        top: '50%',
-                        left: '50%',
-                        transform: 'translate(-50%, 35vh)',
-                        fontSize: '20px',
-                        cursor: 'pointer',
-                        width: 'max-content',
-                    }}>
-                        ver todos los arquitectos
-                    </div>
                 </div>
             </div>
             <div style={{ display: 'flex', justifyContent: 'center', flexDirection: "column", alignItems: 'center', transform: "translateY(50px)", zIndex: 1 }}>
