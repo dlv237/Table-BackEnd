93e5d878f78dd8bf2edd41984928512236dc6b4b
dlv237 <dolobos@uc.cl>
2024-06-27 00:40:36 -0400

    fix

--- my-app/src/pages/search.tsx
+++ my-app/src/pages/search.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Footer from '../components/general/footer';
 
 export default function Search() {
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollPosition(window.pageYOffset);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
     return (
-        <div style={{ position: 'relative', minHeight: '100vh', overflow: "hidden" }}>
+        <div style={{ position: 'relative', minHeight: '100vh', overflowY: "hidden" }}>
             <div className="container" style={{ position: 'relative', zIndex: 1 }}>
                 <div className="subContainer">
                     <div className="logoContainerSmall" style={{ zIndex: "1", cursor: "pointer" }} onClick={() => window.location.href = '/'}>
