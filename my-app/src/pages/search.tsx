87a4f0a34feca14c49e0f407306203f4f3a7f0b6
dlv237 <dolobos@uc.cl>
2024-07-01 22:17:11 -0400

    chore(ui): fixed search view

--- my-app/src/pages/search.tsx
+++ my-app/src/pages/search.tsx
@@ -2,32 +2,21 @@ import React, { useEffect, useState } from 'react';
 import Footer from '../components/general/footer';
 
 export default function Search() {
-    // Initialize isWide with a default value (false) or a more appropriate initial state for your use case
     const [isWide, setIsWide] = useState(false);
-    const [windowHeight, setWindowHeight] = useState(0);
 
     useEffect(() => {
-        // Check if window is defined (this is for SSR/SSG compatibility)
         if (typeof window !== "undefined") {
-            // Set the initial state based on window.innerWidth
             setIsWide(window.innerWidth > 1020);
-            setWindowHeight(window.innerHeight);
 
             const handleResize = () => {
                 setIsWide(window.innerWidth > 1020);
-                setWindowHeight(window.innerHeight);
             };
 
             window.addEventListener('resize', handleResize);
 
-            // Clean up the event listener on component unmount
-            
             return () => window.removeEventListener('resize', handleResize);
         }
-    }, []); // Empty dependency array means this effect runs once on mount
-
-    const topPositionBuscar = windowHeight / 2 + 0.3*windowHeight; // Ajusta estos valores según sea necesario
-    const topPositionVerTodos = windowHeight / 2 - 0.35*windowHeight; // Ajusta estos valores según sea necesario
+    }, []);
 
     return (
         <div className='container' 
@@ -43,8 +32,8 @@ export default function Search() {
                     <img src="/LOGO_TEXTO.png" alt="Logo" className="centeredImageSmall" />
                 </div>
                 <div style={{display: 'flex', flexDirection: "column", alignItems: "center"}}>
-                <h1 style={{position: "absolute", top: `${topPositionBuscar}px`, fontSize: "x-large"}}>buscar arquitectos</h1>
-                <h1 style={{position: "absolute", top: `${topPositionVerTodos}px`, fontSize: "x-large"}}>ver todos los arquitectos</h1>
+                    <h1 style = {{position: "absolute", top: "50%", transform: "translateY(30vh)", fontSize: "x-large"}}>buscar arquitectos</h1>
+                    <h1 style = {{position: "absolute", top: "50%", transform: "translateY(-35vh)", fontSize: "x-large"}}>ver todos los arquitectos</h1>
                 </div>
                 
             </div>
