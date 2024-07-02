df530738a53d3288ae5e17b3c796029acaec355b
dlv237 <dolobos@uc.cl>
2024-07-01 22:52:28 -0400

    chore(ui): fixed search view

--- my-app/src/pages/search.tsx
+++ my-app/src/pages/search.tsx
@@ -3,9 +3,10 @@ import Footer from '../components/general/footer';
 
 export default function Search() {
     const [isWide, setIsWide] = useState(false);
-    const [availableHeight, setAvailableHeight] = useState(window.innerHeight);
+    const [availableHeight, setAvailableHeight] = useState(0);
 
     useEffect(() => {
+        // Verificar si estamos en el cliente
         if (typeof window !== "undefined") {
             const handleResize = () => {
                 setIsWide(window.innerWidth > 1020);
@@ -14,7 +15,8 @@ export default function Search() {
 
             window.addEventListener('resize', handleResize);
 
-            handleResize(); // Llamar una vez para establecer el estado inicial
+            // Llamar a handleResize una vez para establecer el estado inicial
+            handleResize();
 
             return () => window.removeEventListener('resize', handleResize);
         }
@@ -24,7 +26,7 @@ export default function Search() {
         <div className='container' 
             style={{
                 backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.65), rgba(255, 255, 255, 0.65)), url(/LOGO_TABLE_ROTADO_ST.png)`, 
-                height: availableHeight, // Utiliza el estado para la altura
+                height: `${availableHeight}px`, // Utiliza el estado para la altura
                 backgroundSize: isWide ? "contain" : "cover", 
                 backgroundPosition: "center",
                 backgroundRepeat: "no-repeat",
@@ -34,8 +36,8 @@ export default function Search() {
                     <img src="/LOGO_TEXTO.png" alt="Logo" className="centeredImageSmall" />
                 </div>
                 <div style={{display: 'flex', flexDirection: "column", alignItems: "center"}}>
-                    <h1 style = {{position: "absolute", top: availableHeight * 0.8, fontSize: "x-large"}}>buscar arquitectos</h1>
-                    <h1 style = {{position: "absolute", top: availableHeight * 0.15, fontSize: "x-large"}}>ver todos los arquitectos</h1>
+                    <h1 style = {{position: "absolute", top: `${availableHeight * 0.8}px`, fontSize: "x-large"}}>buscar arquitectos</h1>
+                    <h1 style = {{position: "absolute", top: `${availableHeight * 0.15}px`, fontSize: "x-large"}}>ver todos los arquitectos</h1>
                 </div>
             </div>
             <Footer />
