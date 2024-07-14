251e680e348b9a995084d68977a4ad3d9a967bb9
dlv237 <dolobos@uc.cl>
2024-07-14 16:03:44 -0400

    feat: added about base page

--- my-app/src/pages/about.tsx
+++ my-app/src/pages/about.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import Footer from '@/components/general/footer';
+import '@/app/about.css';
 
 const About = () => (
-    <div>
-        <h1>Hola usuario</h1>
+    <div style={{position: 'relative'}}>
+        <div className="container">
+            <div className="logoContainer">
+                <img src="/LOGO_TEXTO.png" alt="Logo" className="centeredImage"/>
+            </div>
+            <h1 className="title">tu mesa de proyectos</h1>
+            <div className="aboutUsContainer">
+                "Aquí va la información sobre table (a actualizar), puede incluir fotos y cualquier información pertinente."
+            </div>
+        </div>
+        <div style={{display: 'flex', justifyContent: 'center', flexDirection: "column", alignItems: 'center'}}>
+            <Footer />
+        </div>
     </div>
 )
 
